914c11d71e2443944616ea77a9e67caae5d48f5b
Alexander Méndez <djelalex@gmail.com>
2016-07-04 13:06:00 -0400

    Add files via upload

--- resources/config.php
+++ resources/config.php
@@ -1,16 +1,7 @@
 <?php
-    session_start();
-
-    error_reporting(E_ALL ^ E_DEPRECATED);
 
     $conexion = mysql_connect("localhost", "root", "");
     mysql_select_db("saie", $conexion);
     mysql_query("SET NAMES 'utf8'");
-
-
-    /* Redirect */
-    if (!isset($_SESSION['usuario'])) {
-        header("Location: login.php?from=config");
-    }
 
 ?>
